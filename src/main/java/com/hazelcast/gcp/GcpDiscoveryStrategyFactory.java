77bdb55f3df558e7debabe1dcf7467e02aa527b0
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-08-04 16:25:16 +0300

    Implement auto-detection

--- src/main/java/com/hazelcast/gcp/GcpDiscoveryStrategyFactory.java
+++ src/main/java/com/hazelcast/gcp/GcpDiscoveryStrategyFactory.java
@@ -50,4 +50,47 @@ public class GcpDiscoveryStrategyFactory
         }
         return result;
     }
+
+    @Override
+    public boolean isAutoDetectionApplicable() {
+        return googleInternalDnsConfigured() && metadataFlavorGoogle();
+    }
+
+
+    private String readFileContents(String fileName) {
+        InputStream is = null;
+        try {
+            File file = new File(fileName);
+            byte[] data = new byte[(int) file.length()];
+            is = new FileInputStream(file);
+            is.read(data);
+            return new String(data, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get " + fileName, e);
+        } finally {
+            IOUtil.closeResource(is);
+        }
+    }
+
+    private boolean googleInternalDnsConfigured() {
+        return readFileContents("/etc/resolv.conf").contains("google.internal") ||
+                readFileContents("/etc/hosts").contains("google.internal");
+    }
+
+    private boolean metadataFlavorGoogle() {
+        return isEndpointAvailable("metadata.google.internal");
+    }
+
+    private boolean isEndpointAvailable(String url) {
+        return !RestClient.create(url)
+                .get()
+                .isEmpty();
+    }
+
+    @Override
+    public DiscoveryStrategyLevel discoveryStrategyLevel() {
+        return DiscoveryStrategyLevel.CLOUD_VM;
+    }
+
+
 }
