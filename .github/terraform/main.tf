19da2248a97dcec28cb46fd235854d16ce99618f
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-08-27 12:38:03 +0300

    Fix Terraform and gcp versions

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.35.0"
-
+      version = "= 3.35.0"
     }
   }
   required_version = ">= 0.13"
